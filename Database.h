77ccd390365ffcdd112dc13c30f7222f6e05dfc8
Steven <stevenrogers52software@gmail.com>
2016-05-09 10:17:08 -0400

    spacing

--- Database.h
+++ Database.h
@@ -20,14 +20,14 @@
 #define _CONCAT3_(A,B,C) "CONCAT( " A " , " B " , " C " )"
 #define _OFFSET_         "LIMIT %d,1"
 
-#define FORMAT_STRING_ARGS(format, output, len)	\
-{												\
-	va_list ap;									\
-	char szQuery[len];							\
-	va_start(ap, format);						\
-	vsprintf(szQuery, format, ap);				\
-	va_end(ap);									\
-	output = szQuery;							\
+#define FORMAT_STRING_ARGS(format, output, len) \
+{                                               \
+	va_list ap;                                 \
+	char szQuery[len];                          \
+	va_start(ap, format);                       \
+	vsprintf(szQuery, format, ap);              \
+	va_end(ap);                                 \
+	output = szQuery;                           \
 }
 
 // Callback results are in the same queue as QueueExecuteQuery and CommitManyQueries
@@ -61,12 +61,12 @@ class Database
 
         bool Uninitialise();
         bool Initialize(const char* infoString);
-        bool QueueExecuteQuery(const char* format, ...) ATTR_PRINTF(2, 3);
-        bool ExecuteQueryInstant(const char* format, ...) ATTR_PRINTF(2, 3);    
+        bool QueueExecuteQuery(const char* format, ...);
+        bool ExecuteQueryInstant(const char* format, ...);    
         
         int32 QueryInt32(const char* format, ...);
 
-        std::shared_ptr<QueryResult> Query(const char* format, ...) ATTR_PRINTF(2, 3);
+        std::shared_ptr<QueryResult> Query(const char* format, ...);
 
         operator bool () const { return m_pMYSQL != NULL; }
         
